d2b9c8645b8c3b94febc3b4b73dd594d3fd964cc
YoheiIguchi <matthew.at.muse@gmail.com>
2021-01-05 18:26:13 +0900

    added publicpath

--- packages/container/config/webpack.prd.js
+++ packages/container/config/webpack.prd.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const PrdConfig = {
   mode: "production",
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
